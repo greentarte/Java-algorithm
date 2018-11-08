991d8f67d2d19c8b9e70c8a4dd2848f0a36150cb
greentarte <35029574+greentarte@users.noreply.github.com>
2018-11-08 19:16:20 +0900

    Update Kth_number.java

--- src/programmers/Kth_number.java
+++ src/programmers/Kth_number.java
@@ -6,10 +6,7 @@ public class Kth_number {
 
 	public static void main(String[] args) {
 		int[] array= {1,5,2,6,3,7,4};
-		int[][] commands = { {2,5,3},
-							 {4,4,1},
-							 {1,7,3}
-		};
+		int[][] commands = { {2,5,3}, {4,4,1}, {1,7,3} };
 		int[] answer=solution(array,commands);
 		for(int i=0; i<answer.length; i++) {
 			System.out.print(answer[i]+" ");
