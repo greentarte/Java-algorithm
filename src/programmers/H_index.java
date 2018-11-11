81bb7edba580fcbce5cc7151c04f657f89b2dcf2
greentarte <35029574+greentarte@users.noreply.github.com>
2018-11-11 11:13:26 +0900

    Update H_index.java

--- src/programmers/H_index.java
+++ src/programmers/H_index.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class H_index {
 
@@ -9,24 +10,24 @@ public class H_index {
 	public static int solution(int[] citations){
 		 	int answer = 0;
 	        int cnt=0;
-	        int h=0;
 	        int length=citations.length;
-	        
+	        Arrays.sort(citations);
+	        int h=citations[length-1];
 	        while(true){
-	             for(int i=0; i<length; i++){
+	             for(int i=length-1; i>=0; i--){
 	                if(citations[i]>=h){
 	                    cnt++;
 	             }
 	                }
 	            
 	            if(cnt>=h&& length-h<h){
-	                 if(answer<h){
+	                 if(answer>h){
 	                	 answer=h;
 	                 }
 	                break;
 	            } else{
 	            	answer=h;
-	                h++;
+	                h--;
 	                cnt=0;
 	            }
 	        }
