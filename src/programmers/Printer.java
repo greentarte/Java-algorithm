870d32c2956f731f5c224afa5ebdc5641a378fb0
greentarte <35029574+greentarte@users.noreply.github.com>
2018-11-15 17:23:48 +0900

    Add files via upload

---
+++ src/programmers/Printer.java
@@ -0,0 +1,42 @@
+package programmers;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Printer {
+
+	public static void main(String[] args) {
+		int[] priorities= {2, 1, 3, 2};
+		int location = 2;
+		System.out.println(solution(priorities,location));
+
+	}
+	public static int solution(int[] priorities, int location) {
+		int answer=0;
+		char start='A';
+		String[] result=new String[priorities.length];
+		for(int i=0; i<result.length; i++) {
+			result[i]=priorities[i]+""+(start++);
+//			System.out.println(result[i]);
+			
+		}
+//		System.out.println("-----------------------------");
+		Arrays.sort(result); //오름차순 정렬
+//		for(int i=0; i<result.length; i++) {
+//			System.out.println(result[i]);
+//		}
+		//A 65
+		//location 2 -> C 67
+		//탐색조건 65+location
+		start=(char)(start+location);
+		for (int i = result.length-1; i >-1 ; i--) {
+			if(result[i].substring(1,1).equals(start+"")){
+				answer=result.length-i+1;
+			}
+		}
+
+		return answer;
+	}
+	
+}
